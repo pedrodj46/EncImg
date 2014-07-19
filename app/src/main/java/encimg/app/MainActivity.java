3eb3bd7ec7ed3cb73fdde2982ab127cedde2cd46
pedrodj46 <pedrodj46@gmail.com>
2014-07-19 16:06:02 +0200

    tolto contatore

--- app/src/main/java/encimg/app/MainActivity.java
+++ app/src/main/java/encimg/app/MainActivity.java
@@ -1,8 +1,6 @@
 package encimg.app;
 
 import android.support.v7.app.ActionBarActivity;
-
-import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
@@ -17,17 +15,13 @@ public class MainActivity extends ActionBarActivity {
 
     private EditText  username=null;
     private EditText  password=null;
-    private TextView attempts;
     private Button login;
-    int counter = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         username = (EditText)findViewById(R.id.editText1);
         password = (EditText)findViewById(R.id.editText2);
-        attempts = (TextView)findViewById(R.id.textView5);
-        attempts.setText(Integer.toString(counter));
         login = (Button)findViewById(R.id.button1);
     }
 
@@ -40,13 +34,6 @@ public class MainActivity extends ActionBarActivity {
         else{
             Toast.makeText(getApplicationContext(), "Wrong Credentials",
                     Toast.LENGTH_SHORT).show();
-            attempts.setBackgroundColor(Color.RED);
-            counter--;
-            attempts.setText(Integer.toString(counter));
-            if(counter==0){
-                login.setEnabled(false);
-            }
-
         }
 
     }
